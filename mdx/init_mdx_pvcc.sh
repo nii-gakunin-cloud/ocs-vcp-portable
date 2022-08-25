36e122d02351c3c47029e1cce5445981c75f739c
nii-gakunin-cloud <vcp-support@nii.ac.jp>
2022-08-25 20:49:59 +0900

    handson 22.09 docker-compose

--- mdx/init_mdx_pvcc.sh
+++ mdx/init_mdx_pvcc.sh
@@ -26,8 +26,9 @@ sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io
 sudo docker version
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose 
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose 
+sudo apt-get install -y docker-compose
 
 # setup VC Controller
 cd $(dirname $0)/..
