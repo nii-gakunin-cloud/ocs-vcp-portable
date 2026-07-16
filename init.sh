5597b992a86608d2d80974eb501e3d8e109c6e6c
terayamaaaa <terayama@ascade.co.jp>
2026-07-16 13:04:15 +0900

    fix: Fix deploy scripts
    - Use template in nginx to support environment var
    - Create dummy certs in init.sh and not docker compose up
    - Fix vault not start because of permission

--- init.sh
+++ init.sh
@@ -2,12 +2,21 @@
 
 CIDR=$(ip -o -4 addr show | grep -v ' lo' | awk '{print $4}' | head -n 1)
 IP=${CIDR%/*}
-cat << EOF > .env
+
+if [ ! -e .env ]; then
+    cat << EOF > .env
 GF_SECURITY_ADMIN_PASSWORD=$(cat /dev/urandom | base64 | fold -w 10 | head -n 1)
 CONSUL_INITIAL_TOKEN=$(uuidgen)
 VCP_VCC_PRIVATE_IPMASK=$CIDR
 SERF_ADVERTISE=$IP
 EOF
+fi
 
-docker compose pull
-docker compose up -d
+if [ ! -d cert ]; then
+    sh ./tools/create_dummy_cert.sh
+    chown root:1000 ./cert/occtr.key
+    chmod 640 ./cert/occtr.key
+fi
+
+mkdir -p ./vault/data
+# docker compose up -d --scale worker=3
