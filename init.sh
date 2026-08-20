f281335cf01c514e396126b838a2f4e42707411f
terayamaaaa <terayama@ascade.co.jp>
2026-08-20 18:27:18 +0900

    fix: Update init script

--- init.sh
+++ init.sh
@@ -27,6 +27,7 @@ fi
 
 if [ ! -e .env ]; then
     cat << EOF > .env
+OCCTR_IMAGE=harbor.vcloud.nii.ac.jp/vcp/occtr:26.10.0
 GF_SECURITY_ADMIN_PASSWORD=$(cat /dev/urandom | base64 | fold -w 10 | head -n 1)
 CONSUL_INITIAL_TOKEN=$(uuidgen)
 VCP_VCC_PRIVATE_IPMASK=$CIDR
@@ -38,10 +39,11 @@ fi
 # Create cert
 if [ ! -d "$CERT_DIR" ]; then
     bash tools/create_dummy_cert.sh "$CERT_DIR" 3600
-    chown root:1000 "$CERT_DIR/occtr.key"
-    chmod 640 "$CERT_DIR/occtr.key"
 fi
+chown root:1000 "$CERT_DIR/occtr.key"
+chmod 640 "$CERT_DIR/occtr.key"
 
 mkdir -p ./vault/data
 chown 1000:1000 ./vault/data
+chmod 770 ./vault/data
 # docker compose up -d --scale worker=3
